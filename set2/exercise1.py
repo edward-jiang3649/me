83e75712e842184ee209aa441ceb349c6172ab2a
Edward Jiang <edward.jiang3649@gmail.com>
2021-06-11 14:15:39 +1000

    Tute 2 save

--- set2/exercise1.py
+++ set2/exercise1.py
@@ -11,19 +11,25 @@ import platform
 # I think this will print "hello! Let's get started" by calling the print function.
 print("hello! Let's get started")  # it printed "hello! Let's get started"
 
+#This line is a function with some_words being the function with a list containing'what', 'does', 'this', 'line', 'do', '?' the [] is used for defining lists and indexing into all collections
 some_words = ['what', 'does', 'this', 'line', 'do', '?']
 
+#this will print "word" when "word" is called from the some_words list
 for word in some_words:
     print(word)
 
+#this will print "x" when "x" is called from the some_words list
 for x in some_words:
     print(x)
 
+#this will print out the list 'some_words'
 print(some_words)
 
+#if there is more than 3 words in 'some_words' it will print 'some_words contains more than 3 words'
 if len(some_words) > 3:
     print('some_words contains more than 3 words')
 
+#this will get the information about current operating system (or of the)
 def usefulFunction():
     """
     You may want to look up what uname does before you guess
